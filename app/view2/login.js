3440b257a320cc2816f9043e2bdc57662271fdee
sravankumar <s521731@mail.nwmissouri.edu>
2015-07-15 01:26:54 -0500

    select all added

--- app/view2/login.js
+++ app/view2/login.js
@@ -14,6 +14,7 @@ login.controller('loginContrl', ['$scope', function ($scope) {
                 $scope.error = 'Password Required';
             }else if ($scope.loginForm.Email && $scope.loginForm.Password ) {
                 $scope.regSuccess = 'Log In Successful';
+                $location.path( "view2.html" );
               
 
             }
